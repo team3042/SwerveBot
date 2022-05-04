6901a0b016e51de2864b61d27bccedc70418d877
Team3042 <team3042@gmail.com>
2022-05-03 20:44:55 -0500

    SwerveModule
    started to write swerve module code

---
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/SwerveModule.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team3042.robot.subsystems;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.AnalogInput;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+
+public class SwerveModule {
+
+    private final CANSparkMax driveMotor;
+    private final CANSparkMax turningMotor;
+
+    private final RelativeEncoder driveEncoder;
+    private final RelativeEncoder turningEncoder;
+
+    private final PIDController turningPidController;
+
+    private final CANCoder absoluteEncoder;
+    private final boolean absoluteEncoderReversed;
+    private final double absoluteEncoderOffsetRad;
+
+    public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+                        int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+
+        this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+        this.absoluteEncoderReversed = absoluteEncoderReversed;
+        absoluteEncoder = new CANCoder(absoluteEncoderId);
+
+        driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
+        turningMotor = new CANSparkMax(turningMotorId, MotorType.kBrushless);
+
+        driveMotor.setInverted(driveMotorReversed);
+        turningMotor.setInverted(turningMotorReversed);
+
+        driveEncoder = driveMotor.getEncoder();
+        turningEncoder = turningMotor.getEncoder();
+        
+
+        driveEncoder.setPositionConversionFactor(factor);
+        driveEncoder.setVelocityConversionFactor(factor);
+        turningEncoder.setPositionConversionFactor(factor);
+        turningEncoder.setVelocityConversionFactor(factor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
